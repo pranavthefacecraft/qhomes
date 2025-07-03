2bbc65ff96142b86bf9f6caeb4688132ab73c4e6
abhicominin <abhijeetkushwah114@gmail.com>
2025-07-03 17:16:26 +0530

    changes again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={
-            <main className="container mx-auto px-2 py-4">
+            <main className="container-full mx-auto px-2 py-4">
               {/* Headings at the top center */}
               <div className="w-full items-center">
                 <h1 className="text-3xl md:text-4xl font-bold text-center text-gray-800 mb-2">
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -18,9 +18,109 @@ const Map = () => {
 
   const options = useMemo(
     () => ({
-      mapId: "2d55830b3d259e645a1dd60a",
       disableDefaultUI: true,
       clickableIcons: false,
+      styles : [
+    {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.attraction",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.government",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
     }),
     []
   );
@@ -90,17 +190,31 @@ const Map = () => {
     />
     {mapRef &&
       markers.map(marker => (
-       <CustomMarker key={marker.id} map={mapRef} position={marker.position}>
-            <div
-              className={`bg-white select-none text-grey rounded-full shadow-lg px-1 py-0 min-w-[20px] min-h-[16px] border border-gray-200 flex items-center justify-center transition-colors duration-200 ${
-                hoveredPropertyId === marker.id ? 'bg-black text-white border-black' : ''
-              }`}
-            >
-              <span className="text-xs font-bold flex items-center">
-                {marker.currency && <span>{marker.currency}&nbsp;</span>}
-                <span>{marker.price.toLocaleString()}</span>
-              </span>
-            </div>
+      <CustomMarker key={marker.id} map={mapRef} position={marker.position}>
+        <div
+          className={`bg-white select-none text-grey rounded-full shadow-lg px-1 py-0 min-w-[20px] min-h-[16px] border border-gray-200 flex items-center justify-center transition-colors duration-200 ${
+            hoveredPropertyId === marker.id ? 'bg-black text-white border-black' : ''
+          }`}
+        >
+        
+        <span className="labels text-xs font-bold flex items-center">
+          {/* Custom icon SVG */}
+        <svg
+          className="w-6 h-6 mr-4"
+          stroke="currentColor"
+          strokeWidth={1.5}
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5A2.5 2.5 0 1 1 12 6a2.5 2.5 0 0 1 0 5.5z"
+          />
+        </svg>
+          {marker.currency && <span>{marker.currency}&nbsp;</span>}
+          <span>{marker.price.toLocaleString()}</span>
+        </span>
+        </div>
       </CustomMarker>
       ))}
   </div>
