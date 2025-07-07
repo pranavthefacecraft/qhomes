d65038a0816a972fab8fd1c61c544d11d5118ea4
abhicominin <abhijeetkushwah114@gmail.com>
2025-07-08 00:43:14 +0530

    temp cards

--- frontend/src/components/NewMarker.jsx
+++ frontend/src/components/NewMarker.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { OverlayView } from "@react-google-maps/api";
 
-const NewMarker = ({ position, children, hovered }) => (
+const NewMarker = ({ position, children, hovered, onClick }) => (
   <OverlayView
     position={position}
     mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
   >
-    <div className={`tooltip-container${hovered ? " hovered" : ""}`}>
+    <div
+      className={`tooltip-container${hovered ? " hovered" : ""}`}
+      onClick={onClick}
+      style={{ cursor: "pointer", pointerEvents: "auto" }} // <-- Ensure pointerEvents is auto
+    >
       <div className="tooltip-bubble">{children}</div>
       <div className="tooltip-pointer-border"></div>
       <div className="tooltip-pointer"></div>
